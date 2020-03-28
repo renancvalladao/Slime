6fe2b910ee50f0237039287a96e7921406f0a275
Mozzert <62816641+Mozzert@users.noreply.github.com>
2020-03-28 20:09:20 -0300

    Teste

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -13,7 +13,7 @@ public class CameraMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Test Github
     }
 
     // Update is called once per frame
